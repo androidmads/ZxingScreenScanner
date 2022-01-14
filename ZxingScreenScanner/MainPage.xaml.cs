5da623b942812f8a559a2fff9b10caf3aff64b39
Mushtaq M A <mushtaqat3gb@gmail.com>
2022-01-15 01:25:36 +0530

    Add project files.

---
+++ ZxingScreenScanner/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ZxingScreenScanner
+{
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        zxing.OnScanResult += (result) =>
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                lblResult.Text = result.Text;
+            });
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        zxing.IsScanning = true;
+    }
+
+    protected override void OnDisappearing()
+    {
+        zxing.IsScanning = false;
+
+        base.OnDisappearing();
+    }
+}
+}
